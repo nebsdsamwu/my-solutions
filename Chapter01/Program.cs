b3c83fb0e839a83c730a286fa853ec0701aad1fb
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-08-09 23:27:02 -0700

    01-01

--- Chapter01/Program.cs
+++ Chapter01/Program.cs
@@ -10,12 +10,43 @@ namespace Chapter01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(IsUniqueNSqr("aobcdfgeo"));
+            Console.ReadKey();
         }
 
-        static bool IsUnique(string a, string b)
+        static bool IsUnique(string str)
         {
-            bool isUnique = false;
-            return isUnique;
+            char[] chs = str.ToCharArray();
+            int[] chArchive = new int[128];
+
+            for (int i = 0; i < chs.Length; i++)
+            {
+                if (chArchive[(int)chs[i]] == 0)
+                {
+                    chArchive[(int)chs[i]] = 1;
+                }
+                else if (chArchive[(int)chs[i]] == 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool IsUniqueNSqr(string str)
+        {
+            char[] chs = str.ToCharArray();
+
+            for (int i = 0; i < chs.Length; i++)
+            {
+                for (int j=i + 1; j< chs.Length; j++)
+                {
+                    if (chs[i] == chs[j])
+                        return false;
+                }
+            }
+            return true;
         }
     }
 }
