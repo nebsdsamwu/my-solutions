aebb6e95c99c5aa067dd21bd5f33002f7cf6f427
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-08-09 22:27:47 -0700

    first commit

---
+++ Chapter01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static bool IsUnique(string a, string b)
+        {
+            bool isUnique = false;
+            return isUnique;
+        }
+    }
+}
