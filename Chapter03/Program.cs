6cffd4e4119b19047921ec6e3418886049162836
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-08-29 23:00:46 -0700

    ch3

---
+++ Chapter03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class MyStack<T>
+    {
+        private class StackNode<T>
+        {
+            public T data { get; set; }
+            public StackNode<T> next { get; set; }
+
+            public StackNode(T data)
+            {
+                this.data = data;
+            }
+        }
+
+        private StackNode<T> top;
+
+        public T Pop()
+        {
+            if (top == null) throw new Exception("Stack empty");
+            T item = top.data;
+            top = top.next;
+            return item;
+        }
+
+        public void push(T item)
+        {
+            StackNode<T> t = new StackNode<T>(item);
+            t.next = top;
+            top = t;
+        }
+
+        public T peak()
+        {
+            if (top == null) throw new Exception("stack empty");
+            return top.data; 
+        }
+
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+    }
+}
