402151500389a6077d7c51a7abe737923ff14a81
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-08-30 23:26:08 -0700

    3.1  error

--- Chapter03/Program.cs
+++ Chapter03/Program.cs
@@ -36,14 +36,53 @@ namespace Chapter03
 
     public class ThreeInOne<T>
     {
-        private static int size = 18;
-        private T[] stackArray = new T[size];
-        public T[] StackArray
+        private static int tSize = 18;
+        private static int stkSize = tSize / 3;
+        public static StackNode<T>[] stackArray = new StackNode<T>[tSize];
+
+        public class StackNode<T>
         {
-            get { return stackArray; }
-            set { stackArray = value; }
+            public T data { get; set; }
+            public StackNode<T> next { get; set; }
+
+            public StackNode(T data)
+            {
+                this.data = data;
+            }
         }
 
+        public class OneTriStack<T>
+        {
+            private class OneStack<T>
+            {
+                private StackNode<T> top;
+                public int length {get; set;}
+                public int startIdx { get; set; }
+                public int topIdx { get; set; }
+
+                public OneStack(int index)
+                {
+                    startIdx = index;
+                    topIdx = index;
+                    length = 0;
+                }
+
+                public void Push(T data)
+                {
+                    if (length == stkSize) throw new Exception("Stack full");
+                    StackNode<T> nd = new StackNode<T>(data);
+                    stackArray[topIdx + 1] = nd;
+                    nd.next = top;
+                    top = nd;
+                }
+
+                public bool IsFull()
+                {
+                    return length == stkSize;
+                }
+            }
+
+        }
 
     }
 
@@ -71,14 +110,14 @@ namespace Chapter03
             return item;
         }
 
-        public void push(T item)
+        public void Push(T item)
         {
             StackNode<T> t = new StackNode<T>(item);
             t.next = top;
             top = t;
         }
 
-        public T peak()
+        public T Peak()
         {
             if (top == null) throw new Exception("stack empty");
             return top.data; 
