66449fec142a01b22a11666917de9d082c284845
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-10-03 23:45:23 -0700

    5.0

--- Chapter05/Program.cs
+++ Chapter05/Program.cs
@@ -10,14 +10,19 @@ namespace Chapter05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(SetBit(9, 2));
+            Console.WriteLine(GetBit(13, 2));
+            Console.WriteLine(SetBit(13, 1));
+            Console.WriteLine(ClearBit(13, 2));
+            Console.WriteLine(ClearBitFromI(13, 2));
+            Console.WriteLine(ClearBitToI(13, 2));
+            Console.WriteLine(UpdateBit(13, 1, true));
 
             Console.ReadLine();
         }
 
         public static bool GetBit(int num, int i)
         {
-            // 1001
+            // 1101
             // 0100
 
             return ((num & 1 << i) != 0);
@@ -25,10 +30,46 @@ namespace Chapter05
 
         public static int SetBit(int num, int i)
         {
-            // 1001
-            // 0100
             // 1101
+            // 0110
+            // 1111
             return num | (1 << i);
+        }
+
+        public static int ClearBit(int num, int i)
+        {
+            // 1101
+            // 0010
+            int mask = ~(1 << i);
+            return num & mask;
+        }
+
+        public static int ClearBitFromI(int num, int i)
+        {
+            // 1101
+            // 0011
+            int mask = (1 << i) - 1;
+            return num & mask;
+        }
+
+        public static int ClearBitToI(int num, int i)
+        {
+            // 1101
+            // 1000
+            int mask = (-1 << (i + 1));
+            return num & mask;
+        }
+
+        public static int UpdateBit(int num, int i, bool is1)
+        {
+            // 1101
+            // 1011  
+
+            // 1001
+            // 0000
+            int value = is1 ? 1 : 0;
+            int mask = ~(1 << i);
+            return (num & mask) | (value << i);
         }
     }
 }
