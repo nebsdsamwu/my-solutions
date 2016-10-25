15a10910b372d435a23da4df69efc472051b519e
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-10-25 00:38:06 -0700

    6.12

--- Chapter06/Program.cs
+++ Chapter06/Program.cs
@@ -12,8 +12,47 @@ namespace Chapter06
         {
             //Console.WriteLine(PrimeNaive(111191));
             // Console.WriteLine(PrimeV1(111191));
-            Console.WriteLine(PrimeV1(11119111));
+            //Console.WriteLine(PrimeV1(11119111));
+
+            Console.WriteLine(KidsRatioForFamilies(10000000).ToString("F3"));
+
             Console.ReadKey();
+        }
+
+        static double KidsRatioForFamilies(int n)
+        {
+            double r = 0;
+            double g = 0;
+            double b = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Tuple<int, int> ets = EstimateKidsNums();
+                g += 1;
+                b += ets.Item1;
+            }
+
+            r = (double)(g / (b + g));
+            return r;
+        }
+
+        static Tuple<int, int> EstimateKidsNums()
+        {
+            int b = 0;
+            Random rdm = new Random();
+            bool isGirl = false;
+            while(! isGirl)
+            {
+                isGirl = (rdm.Next(0, 2) == 1);
+                if (isGirl)
+                {
+                    break;
+                }
+                else
+                {
+                    b += 1;
+                }
+            }
+            return new Tuple<int, int>(b, 1);
         }
 
         static bool PrimeV1(int n)
