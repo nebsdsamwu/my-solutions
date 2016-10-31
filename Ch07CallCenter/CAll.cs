9612e7c6d3d52569dceed3945dc4b3dec84a5ef4
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-10-30 17:33:55 -0700

    7.2

---
+++ Ch07CallCenter/CAll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch07CallCenter
+{
+    public class Call
+    {
+        private Position HandlerPosition;
+
+
+    }
+
+    public class Caller
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+}
---
+++ Ch07CallCenter/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch07CallCenter
+{
+    public abstract class Employee
+    {
+        private string name;
+        private int id;
+        private string position;
+        private bool available;
+
+        public string Name 
+        { 
+            get { return name; } 
+            set { name = value; } 
+        }
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public bool Available
+        {
+            get { return available; }
+            set { available = value; }
+        }
+    }
+
+    protected enum Position
+    {
+        Respodent = 0,
+        Manager = 1,
+        Director = 2
+    }
+}
