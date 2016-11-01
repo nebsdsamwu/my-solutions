f49da743b7602ff97e5b6115c7a934ae036d9942
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-10-31 23:55:39 -0700

    8.0

---
+++ Chapter08/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FibonaciiMemo(10));
+
+            Console.ReadLine();
+        }
+
+        static int Fibonacii(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            else return Fibonacii(n - 1) + Fibonacii(n - 2);
+        }
+
+        static int FibonaciiMemo(int n)
+        {
+            return FibonaciiMemo(n, new int[n + 1]);
+        }
+
+        static int FibonaciiMemo(int i, int[] memo)
+        {
+            if (i == 0 || i == 1)
+            {
+                return i;
+            }
+
+            if (memo[i] == 0)
+            {
+                memo[i] = FibonaciiMemo(i - 1, memo) + FibonaciiMemo(i - 2, memo);
+            }
+            return memo[i];
+        }
+    }
+}
