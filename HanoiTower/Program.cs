a48f2e5e3dc0fa26733be9c5e622c5fbc6393fe8
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-11-14 23:10:57 -0800

    tower

---
+++ HanoiTower/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Tower[] towers = new Tower[n];
+            for (int i=0; i < n; i++)
+            {
+                towers[i] = new Tower(i);
+            }
+
+            int diskCnt = 8;
+            for (int i = diskCnt - 1; i >= 0; i--)
+            {
+                towers[0].Add(i);
+            }
+
+            towers[0].MoveDisks(n, towers[2], towers[1]);
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
---
+++ HanoiTower/Tower.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTower
+{
+    class Tower
+    {
+        private Stack<int> disks;
+        private int index;
+        public Tower(int i)
+        {
+            disks = new Stack<int>();
+            index = i;
+        }
+
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        public void Add(int d)
+        {
+            if (!(disks.Count == 0) && disks.Peek() <= d)
+            {
+                Console.WriteLine("Error pacing disk " + d);
+            }
+            else
+            {
+                disks.Push(d);
+            }
+        }
+
+        public void MoveTopTo(Tower t)
+        {
+            int top = disks.Pop();
+            t.Add(top);
+        }
+
+        public void MoveDisks(int n, Tower destination, Tower buffer)
+        {
+            if (n > 0)
+            {
+                MoveDisks(n - 1, buffer, destination);
+                MoveTopTo(destination);
+                buffer.MoveDisks(n - 1, destination, this);
+            }
+        }
+    }
+}
