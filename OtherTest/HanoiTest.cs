b4025889d89ca890cf1c46e2737af3a0cdc59d08
Sam.S.Wu <sam.s.wu@newegg.com>
2016-11-17 09:08:20 -0800

    Revert "hanoi test"
    This reverts commit f3db1f9eb869440c2acef379ffb1ecde0cd2c3ee.

--- OtherTest/HanoiTest.cs
+++ OtherTest/HanoiTest.cs
@@ -40,20 +40,6 @@ namespace OtherTest
             Move2Disks(towers[0], towers[1], towers[2], 2);
         }
 
-        private void MoveDisks(Stack<int> fromTwr, Stack<int> buffTwr, Stack<int> toTwr, int index)
-        {
-            if (fromTwr.Count == 2)
-            {
-                int pickedDisk = fromTwr.Pop();
-                buffTwr.Push(pickedDisk);
-                pickedDisk = fromTwr.Pop();
-                toTwr.Push(pickedDisk);
-                pickedDisk = buffTwr.Pop();
-                toTwr.Push(pickedDisk);
-            }
-            else Console.WriteLine("Invalid disk quantity.");
-        }
-
         private void Move2Disks(Stack<int> fromTwr, Stack<int> buffTwr, Stack<int> toTwr, int index)
         {
             if (fromTwr.Count == 2)
